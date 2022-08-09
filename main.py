da6726802c78d863da036803a9e1dbc26b9e09b7
ElmoReno90 <sergio.cofre2011@gmail.com>
2022-08-09 12:58:30 -0400

    Version 4, consumiendo API (no funciona aun)

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import json
+
+import modelo as modelo
 import requests
-import conf
+import admpw
 
 sandbox = "https://sandboxapicdc.cisco.com"
 
-
-def obtener_token(usuario, clave):
+def obtener_token(user, passwd):
     url = sandbox + "/api/aaaLogin.json"
     body = {
         "aaaUser": {
             "attributes": {
-                "name": usuario,
-                "pwd": clave
+                "name": user,
+                "pwd": passwd
             }
         }
     }
@@ -22,6 +23,22 @@ def obtener_token(usuario, clave):
     respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
-token = obtener_token(conf.usuario, conf.clave)
+token = obtener_token(admpw.user, admpw.passwd)
 
-print(token)
+# GET http://apic-ip-address/api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json
+
+def sensor3():
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    elmocookie = {
+        "APIC-Cookie": obtener_token(admpw.user, admpw.passwd)
+    }
+
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.get(sandbox + "api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json", headers=cabecera, cookies=elmocookie, verify=False)
+    vendor = respuesta.json()["imdata"][i]["eqptSensor"]["attributes"]["vendor"]
+    print(vendor)
+
+sensor3()
+
