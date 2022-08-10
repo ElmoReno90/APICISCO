accf5cd4518e63dfed4f65d7eae559ce7cff3425
ElmoReno90 <sergio.cofre2011@gmail.com>
2022-08-09 20:09:55 -0400

    Version 5, consumiendo API 2(no funciona aun)

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
-
-import modelo as modelo
 import requests
 import admpw
 
 sandbox = "https://sandboxapicdc.cisco.com"
+
 
 def obtener_token(user, passwd):
     url = sandbox + "/api/aaaLogin.json"
@@ -23,9 +22,32 @@ def obtener_token(user, passwd):
     respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
+
+
 token = obtener_token(admpw.user, admpw.passwd)
 
-# GET http://apic-ip-address/api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json
+
+# GET https://apic-ip-address/api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json
+
+def top_system():
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    galleta = {
+        "APIC-Cookie": obtener_token(admpw.user, admpw.passwd)
+    }
+
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.get(sandbox + "/api/class/topSystem.json", headers=cabecera, cookies=galleta, verify=False)
+
+    total_nodos = int(respuesta.json()["totalCount"])
+
+    for i in range(0, total_nodos):
+        dn_local = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["dn"]
+
+
+top_system()
+
 
 def sensor3():
     cabecera = {
@@ -34,11 +56,12 @@ def sensor3():
     elmocookie = {
         "APIC-Cookie": obtener_token(admpw.user, admpw.passwd)
     }
-
     requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(sandbox + "api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json", headers=cabecera, cookies=elmocookie, verify=False)
-    vendor = respuesta.json()["imdata"][i]["eqptSensor"]["attributes"]["vendor"]
-    print(vendor)
+    respuesta = requests.get(sandbox + "/api/class/firmware:CtrlrFwStatusCont.json?"
+                                       "query-target=subtree"
+                                       "target-subtree-class=firmwareCtrlrRunning"
+                             , headers=cabecera, cookies=elmocookie, verify=False)
+    print(respuesta)
+
 
 sensor3()
-
